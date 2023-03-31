3f592520496405965e455ee56a85a5042ae1ef38
shaykot43500 <hossainshaykot@gmail.com>
2023-04-01 05:30:31 +0600

    added question compo

--- src/components/Blog/BlogSide.jsx
+++ src/components/Blog/BlogSide.jsx
@@ -14,17 +14,17 @@ const BlogSide = ({ sideblog, markblog }) => {
     }
 
     return (
-        <div className="">
+        <div >
             <div className="bg-gray-100 rounded text-center h-10 py-1 mb-4">
                 <h1 className="text-blue-700 text-lg font-medium">Spent Time On Read : {total} min </h1>
             </div>
 
-            <div className="bg-gray-100 px-2 py-1 rounded">
+            <div className="bg-gray-100 px-2 py-1 rounded mb-4">
                 <p className="text-lg font-medium px-2">Bookmark Blogs : {blogtotal}</p>
                 {markblog.map((title, id) => (
                     <div className="border bg-white shadow-lg rounded mt-2 ">
                         <div className=" text-lg font-medium px-2 py-4  ">
-                            <p className="  ">{title.title}</p>
+                            <p>{title.title}</p>
                         </div>
                     </div>
                 )
@@ -33,6 +33,8 @@ const BlogSide = ({ sideblog, markblog }) => {
 
                 }
             </div>
+
+            
         </div>
     );
 };
---
+++ src/components/Question/Question.jsx
@@ -0,0 +1,72 @@
+import React from 'react';
+
+const Question = () => {
+    return (
+        <div>
+            <div className='bg-gray-100 px-2 py-1 rounded'>
+                    <div className="border bg-white shadow-lg rounded mt-2 px-2 py-4 ">
+                        <p> 1. Difference Between props and state?</p>
+                        <p className='underline'>Ans:</p>
+                        <p>
+                            Props:  
+                            <li>Props are read-only.</li>
+                            <li>Props are immutable.</li>
+                            <li>Props make components reusable.</li>
+
+                            State:
+                            <li>State changes can be asynchronous.</li>
+                            <li>State is mutable.</li>
+                            <li>State cannot make components reusable.</li>
+                        </p>
+                    </div>
+                    <div className="border bg-white shadow-lg rounded mt-2 px-2 py-4 ">
+                        <p> 1. Difference Between props and state?</p>
+                        <p className='underline'>Ans:</p>
+                        <p>
+                            Props:  
+                            <li>Props are read-only.</li>
+                            <li>Props are immutable.</li>
+                            <li>Props make components reusable.</li>
+
+                            State:
+                            <li>State changes can be asynchronous.</li>
+                            <li>State is mutable.</li>
+                            <li>State cannot make components reusable.</li>
+                        </p>
+                    </div>
+                    <div className="border bg-white shadow-lg rounded mt-2 px-2 py-4 ">
+                        <p> 1. Difference Between props and state?</p>
+                        <p className='underline'>Ans:</p>
+                        <p>
+                            Props:  
+                            <li>Props are read-only.</li>
+                            <li>Props are immutable.</li>
+                            <li>Props make components reusable.</li>
+
+                            State:
+                            <li>State changes can be asynchronous.</li>
+                            <li>State is mutable.</li>
+                            <li>State cannot make components reusable.</li>
+                        </p>
+                    </div>
+                    <div className="border bg-white shadow-lg rounded mt-2 px-2 py-4 ">
+                        <p> 1. Difference Between props and state?</p>
+                        <p className='underline'>Ans:</p>
+                        <p>
+                            Props:  
+                            <li>Props are read-only.</li>
+                            <li>Props are immutable.</li>
+                            <li>Props make components reusable.</li>
+
+                            State:
+                            <li>State changes can be asynchronous.</li>
+                            <li>State is mutable.</li>
+                            <li>State cannot make components reusable.</li>
+                        </p>
+                    </div>
+            </div>
+        </div>
+    );
+};
+
+export default Question;
